341d33532135ee4ab2bd6d2d1b824b4a89b551c7
satyavamsi <satyavamsi222.21@gmail.com>
2018-11-14 03:20:23 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  public searchText : string;
+  public banksData : any;
+  bankUrl = 'https://vast-shore-74260.herokuapp.com/banks?city=';
+  cities = ['Hyderabad', 'Bangalore','Delhi', 'Pune', 'Chennai'];
+  loading = false;
   title = 'bankSearchApp';
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+  	this.banksData = [];
+  	this.loading = true;
+  	this.getBanksData(this.cities[0]);
+  }
+
+  getBanksData(bank) {
+  	this.loading = true;
+  	this.banksData = [];
+  	this.http.get(this.bankUrl+bank.toUpperCase())
+  			.subscribe((data) => {
+  				this.loading = false
+  				this.banksData = data
+  			});
+  }
+
 }
